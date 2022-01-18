2ec53e304eca3fe941f5f4a7467efc3ba667e85a
Nathan <nathan.guez@telecomnancy.eu>
2022-01-18 14:18:18 +0100

    fix

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
+from pretraitement import *
 
-
-
-tab50,tab60,tab70,tab75 = pretraitement("household_60min_singleindex.csv");
+tab50,tab60,tab70,tab75 = pretraitement("household_data_60min_singleindex.csv");
+print(tab50)
